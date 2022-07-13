1289e9d0b908509df988075f829ce6947108467a
ruby94412 <ruby94412@gmail.com>
2022-07-13 17:38:54 +0800

    disable ws

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const WebSocketServer = require('ws');
 const express = require('express');
 // Creating a new websocket server
-const wss = new WebSocketServer.Server({port: 8080})
+// const wss = new WebSocketServer.Server({port: 8080})
 const app = express();
 app.listen(3000, () => {
   // const host = app.address().address;
@@ -14,19 +14,19 @@ app.get('/test', (req, res) => {
   })
 })
 // Creating connection using websocket
-wss.on("connection", ws => {
-    console.log("new client connected");
-    // sending message
-    ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
-    });
-    // handling what to do when clients disconnects from server
-    ws.on("close", () => {
-        console.log("the client has connected");
-    });
-    // handling client connection error
-    ws.onerror = function () {
-        console.log("Some Error occurred")
-    }
-});
-console.log("The WebSocket server is running on port 8080");
+// wss.on("connection", ws => {
+//     console.log("new client connected");
+//     // sending message
+//     ws.on("message", data => {
+//         console.log(`Client has sent us: ${data}`)
+//     });
+//     // handling what to do when clients disconnects from server
+//     ws.on("close", () => {
+//         console.log("the client has connected");
+//     });
+//     // handling client connection error
+//     ws.onerror = function () {
+//         console.log("Some Error occurred")
+//     }
+// });
+// console.log("The WebSocket server is running on port 8080");

a56ddec3c4c2e70a21efbd8f039a5ca2fa275119
ruby94412 <ruby94412@gmail.com>
2022-07-15 16:35:25 +0800

    update backend

--- back-end/routes/read.js
+++ back-end/routes/read.js
@@ -1,28 +1,28 @@
 const {Router} = require('express');
-const cors = require('cors');
-const modbusController = require('../controllers/modbusController');
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
-};
+// const cors = require('cors');
+// const modbusController = require('../controllers/modbusController');
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200
+// };
 
 const router = Router();
 
 // Requests 
-router.post('/connection', (req, res) => {
-  console.log(`Got a ${req?.body?.connect ? '' : 'dis'}connection post request`);
-  modbusController.deviceConnection(res, req?.body?.connect);
-});
+// router.post('/connection', (req, res) => {
+//   console.log(`Got a ${req?.body?.connect ? '' : 'dis'}connection post request`);
+//   modbusController.deviceConnection(res, req?.body?.connect);
+// });
 
-router.get('/temperture', cors(corsOptions), (req, res) => {
-  modbusController.readTempertureData(res);
-  console.log("Got a read request");  
-});
+// router.get('/temperture', cors(corsOptions), (req, res) => {
+//   modbusController.readTempertureData(res);
+//   console.log("Got a read request");  
+// });
 
-router.post('/setSVData',  (req, res) => {
-  console.log(`Got a SV data post request: set value to ${req?.body?.svData}`);
-  modbusController.writeSVData(res, req?.body?.svData);
-});
+// router.post('/setSVData',  (req, res) => {
+//   console.log(`Got a SV data post request: set value to ${req?.body?.svData}`);
+//   modbusController.writeSVData(res, req?.body?.svData);
+// });
 
 router.post('/test', (req, res) => {
   res.status(200).send({
